a655f61c9836d71c33c92971ffd5fe449d37c7a4
whitney227 <whitneylcrawford@gmail.com>
2019-06-03 22:37:46 -0400

    created schema file and connected to MySQL Workbench

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,32 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+CREATE DATABASE redeployDB;
+USE redeployDB;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE accounts
+(
+    name VARCHAR (255) NOT NULL,
+    address VARCHAR (255) NOT NULL, 
+    city VARCHAR (255) NOT NULL, 
+    state VARCHAR (255) NOT NULL, 
+    zipcode VARCHAR (255) NOT NULL, 
+    contact_name VARCHAR (255) NOT NULL, 
+    phone_number VARCHAR (255) NOT NULL, 
+    fax_number VARCHAR (255) NOT NULL, 
+    email VARCHAR (255) NOT NULL, 
+    charity BOOLEAN  
+);
+
+CREATE TABLE donations
+(
+    item_description VARCHAR (255) NOT NULL,
+    quantity INT NOT NULL,
+    unit VARCHAR (255) NOT NULL,
+    status VARCHAR (255) NOT NULL
+);
+
+CREATE TABLE login 
+(
+    account_id INT NOT NULL,
+    username VARCHAR (255) NOT NULL,
+    password VARCHAR (255) NOT NULL,
+    PRIMARY KEY (account_id)
+);
