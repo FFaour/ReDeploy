7356242ac1c5ffe7f3e24adc8ad05bd98e9f6cac
whitney227 <whitneylcrawford@gmail.com>
2019-06-04 21:28:17 -0400

    updated donations table with charity column

--- db/schema.sql
+++ db/schema.sql
@@ -20,7 +20,9 @@ CREATE TABLE donations
 (
     item_description VARCHAR (255) NOT NULL,
     quantity INT NOT NULL,
-    unit VARCHAR (255) NOT NULL   
+    unit VARCHAR (255) NOT NULL,
+    charity VARCHAR (255) NOT NULL,  
+    status VARCHAR(255)
 );
 
 CREATE TABLE login 
--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO donations (item_description, quantity, unit) values ('Lidocaine 2%', 10, 'tube');
-INSERT INTO donations (item_description, quantity, unit) values ('Aspirin 81mg 100 count', 12, 'bottle');
-INSERT INTO donations (item_description, quantity, unit) values ('Gloves 250 count', 5, 'box');
-INSERT INTO donations (item_description, quantity, unit) values ('Suture', 4, 'kit');
+INSERT INTO donations (item_description, quantity, unit, charity) values ('Lidocaine 2%', 10, 'tube', 'Red Cross');
+INSERT INTO donations (item_description, quantity, unit, charity) values ('Aspirin 81mg 100 count', 12, 'bottle', 'Doctors without Borders');
+INSERT INTO donations (item_description, quantity, unit, charity) values ('Gloves 250 count', 5, 'box', 'Americares');
+INSERT INTO donations (item_description, quantity, unit, charity) values ('Suture', 4, 'kit', 'Direct Relief');
