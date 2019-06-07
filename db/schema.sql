72462ca5faa62786655b1ea2cc84e90f970c1e5d
whitney227 <whitneylcrawford@gmail.com>
2019-06-06 22:03:59 -0400

    update whitney-test schema

--- db/schema.sql
+++ db/schema.sql
@@ -32,5 +32,5 @@ CREATE TABLE login
     login_id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR (255) UNIQUE NOT NULL,
     password VARCHAR (255) NOT NULL,
-    PRIMARY KEY (login_id)
+    primary key (login_id)
 );
